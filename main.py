0e02912d83e702a9288d0b04e484d913c13d4c17
luxkatana <taseen.bibi@gmail.com>
2026-04-23 22:58:35 +0200

    Kleurenpalette toegepast

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from PIL import Image, ImageEnhance
+from PIL import Image
+import matplotlib.cm
 from math import log2
-from viewport import Viewport, Pixel
+from viewport import Viewport
+
+colormap = matplotlib.cm.get_cmap("viridis").colors
 
 MAX_ITERATION: int = 256
 
@@ -26,20 +29,35 @@ class MandelbrotSet:
         return max(0, min(stable, 1.0)) if clamp else stable
 
 
+def paint(
+    mandelbrot: MandelbrotSet,
+    viewport: Viewport,
+    palette: tuple[int, ...],
+    smooth: bool = True,
+):
+    for pixel in viewport:
+        stability = mandelbrot.stability(complex(pixel), smooth)
+        index = int(min(stability * len(palette), len(palette) - 1))
+        pixel.color = palette[index % len(palette)]
+
+
+def denormalize(palette) -> list[tuple]:
+    return [(tuple(int(channel * 255) for channel in color)) for color in palette]
+
+
+palette = denormalize(colormap)
+
 mandelbrotset = MandelbrotSet()
 
 
-scale = 0.0075
-image = Image.new("L", (512, 512), 1)
+image = Image.new("RGB", (512, 512), 1)
 # for y in range(0, img.height):
 #     for x in range(0, img.width):
 #         complex_number = scale * complex(x - img.width / 2, img.height / 2 - y)
 #         instability: float = 1 - mandelbrotset.stability(complex_number, True)
 #         # Op de schaal van 0 tot 1: Hoe INSTABIEL is de complexe getal?
 #         img.putpixel((x, y), int(instability * 255))
-for pixel in Viewport(image, center=-0.74364 + 0.13182733j, width=0.00012068):
-    c = complex(pixel)
-    instability = 1 - mandelbrotset.stability(c, smooth=True)
-    pixel.color = int(instability * 255)
+viewport = Viewport(image, center=-0.74364 + 0.13182733j, width=0.00012068)
 
+paint(mandelbrotset, viewport=viewport, palette=palette)
 image.show()
